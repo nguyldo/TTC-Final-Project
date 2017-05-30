b2b26940980a3200133f81ff906ac8d05a22f355
michaelxue <mike.xue7@gmail.com>
2017-05-29 17:49:52 -0700

    added fullimageviewcontroller

---
+++ TTCFinalProject/FullImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FullImageViewController.swift
+//  TTCFinalProject
+//
+//  Created by Michael Xue on 5/29/17.
+//  Copyright © 2017 Gametime. All rights reserved.
+//
+
+import UIKit
+
+class FullImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
